c035f280f6bc32290cc1b1b0e0c70148d8c9ed3e
Nanzz <99862432+Nanzz681@users.noreply.github.com>
2022-04-17 12:04:49 +0700

    Create virtex.js

---
+++ plugins/virtex.js
@@ -0,0 +1,15 @@
+let fs = require('fs')
+
+let handler = async (m) => {
+
+let stc = fs.readFileSync('./src/virtex.js')
+
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*🗿*', 'status@broadcast')
+
+}
+
+handler.customPrefix = /^(virtex)$/i
+
+handler.command = new RegExp
+
+module.exports = handler
